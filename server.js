11872c3c185f9f64a4ba8f7cf25a47fb543c8930
sunnykarira <sunnykarira@outlook.com>
2016-02-11 17:40:44 +0530

    Add basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+//app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Server started at port ' + PORT);
+});
