553a20b0245810b6573ec219bb02250ed4570a7d
sunnykarira <sunnykarira@outlook.com>
2016-02-24 22:01:37 +0530

    Add redirection route for GET /main_page

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 var PORT = process.env.PORT || 3000;
 
-
+app.use(morgan('dev'));
 app.use('/', express.static(__dirname + '/public'));
+app.get('/main_page.html', function (req, res){
+	res.redirect('/index.html');
+});
 
 
 app.listen(PORT, function(){
