ad113d56640c72a604d0b9887e8d283f558ed36e
sunnykarira <sunnykarira@outlook.com>
2016-02-25 20:09:16 +0530

    Add GET /index GET /developers GET /contact route

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ var PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use('/', express.static(__dirname + '/public'));
+
 app.get('/main_page.html', function (req, res){
 	res.redirect('/index.html');
 });
 
+app.get('/developers', function (req, res){
+	res.redirect('/developers.html');
+});
+
+app.get('/contact', function (req, res){
+	res.redirect('/contact.html');
+});
+
+app.get('/index', function (req, res){
+	res.redirect('/index.html');
+});
 
 app.listen(PORT, function(){
 	console.log('Server started at port ' + PORT);
